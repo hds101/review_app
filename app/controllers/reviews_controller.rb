93fd68d9d3abf10926ac027abde35c5d3c2941c0
Dmitry Y <hds101@gmail.com>
2020-03-31 14:40:03 +0300

    Basic form fields validation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class ReviewsController < ApplicationController
+  before_action :validate_filter, only: :index
+  before_action :validate_file, only: :upload
+
   def index
     @reviews = Review.all
     @reviews = @reviews.group_by { |review| review.send(filter_group) }
@@ -14,7 +17,7 @@ class ReviewsController < ApplicationController
   def upload
     csv = upload_params[:file]
     ReviewBatchProcessor.call(csv)
-    # TODO: clear flash, update reviews count using ws
+    # TODO: update reviews count using ws
     redirect_to root_path, notice: 'Processing...'
   end
 
@@ -32,5 +35,19 @@ class ReviewsController < ApplicationController
 
   def upload_params
     params.permit(:file)
+  end
+
+  def validate_filter
+    return if filter_params[:format].present? && filter_params[:group_by].present?
+
+    errors = []
+    errors << 'Select report format' if filter_params[:format].blank?
+    errors << 'Select group clause' if filter_params[:group_by].blank?
+
+    redirect_to root_path, notice: errors.join(', ')
+  end
+
+  def validate_file
+    redirect_to root_path, notice: 'Select a file to upload' if upload_params[:file].blank?
   end
 end
